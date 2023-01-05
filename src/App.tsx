71a2ea3a6084b9ac3d8a3b46f61947fa441e8454
Marko Jagetić <marko.jagetic@asynclabs.co>
2023-01-05 20:35:05 +0100

    Add base components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import Navigation from "./components/Navigation";
+import {BrowserRouter, Routes, Route, Outlet} from "react-router-dom";
+import HomeController from "./pages/Home/HomeController";
+import SingleMoviePageController from "./pages/SingleMovie/Application/SingleMoviePageController";
+import MovieDiscoveryPageController from "./pages/MovieDiscovery/Application/MovieDiscoveryPageController"
 
 function App() {
   return (
-    <div className="App"></div>
+      <>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Navigation />}>
+                    <Route index element={<HomeController />} />
+                    <Route path="/discover-movies" element={<MovieDiscoveryPageController />} />
+                    <Route path="/movie/:id" element={<SingleMoviePageController />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+      </>
   );
 }
 
