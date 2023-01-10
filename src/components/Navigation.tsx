ad784226d9c40678120e85235f3b22d09cf34e3b
Marko Jagetić <marko.jagetic@asynclabs.co>
2023-01-10 22:59:19 +0100

    Add Search application requirements

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,24 +1,38 @@
 import React from 'react';
 import AssetManager from "../helpers/AssetManager";
-import { Input, Row, Col, Dropdown, Space } from 'antd';
+import { Row, Col, Dropdown, Space, Button } from 'antd';
 import type { MenuProps } from 'antd';
-import { DownOutlined, SearchOutlined } from '@ant-design/icons';
+import { DownOutlined } from '@ant-design/icons';
 import { Outlet, Link } from "react-router-dom";
+import SearchComponent from "./Search/Application/SearchComponent";
+import MovieRepository from "../pages/SingleMovie/Infrastructure/MovieRepository";
 
-const items: MenuProps['items'] = localStorage.getItem("favorites") ? JSON.parse(localStorage.getItem("favorites")) : [{key: null, label: "Favorite a movie and it'll show up here"}];
+const movieRepository = new MovieRepository();
+
+const items: MenuProps['items'] = localStorage.getItem("favorites") ? movieRepository.fetchFavorites() : [{key: null, label: "Favorite a movie and it'll show up here"}];
 
 const  Navigation = () => {
     return (
         <>
             <Row className="navigation" style={{alignItems: "center", zIndex: "100"}}>
-                <Col lg={18}>
+                <Col lg={12}>
                     <Link to="/">
                         <img src={AssetManager.getUrl("logo.png")} alt="logo" style={{ height: 120 }} />
                     </Link>
                 </Col>
-                <Col lg={6}>
+                <Col lg={12}>
                     <Row>
                         <Space size="large">
+                            <Col>
+                                <Link to="discover-movies">
+                                    <Button>
+                                        Discover
+                                    </Button>
+                                </Link>
+                            </Col>
+                            <Col>
+                                <SearchComponent />
+                            </Col>
                             <Col>
                                 <Dropdown menu={{ items }} placement="bottomRight">
                                     <Link onClick={(e) => e.preventDefault()} to="">
@@ -28,9 +42,6 @@ const  Navigation = () => {
                                         </Space>
                                     </Link>
                                 </Dropdown>
-                            </Col>
-                            <Col>
-                                <Input placeholder="Search for a movie" prefix={<SearchOutlined /> }/>
                             </Col>
                         </Space>
                     </Row>
